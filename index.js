b1fd8949727b167522dd19709a63c48137404bd2
ALIS SANJAYBHAI MANGUKIYA <al819067@dal.ca>
2023-07-07 16:51:46 -0300

    A3

--- index.js
+++ index.js
@@ -1,100 +1,99 @@
-var express = require('express');
-const mysql = require('mysql2/promise');
-const router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
-var app = express();
+const app = express();
+const port = 80;
 
-const db = mysql.createConnection({
-  host: "a3-database-instance-1.couyu5nfg2be.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "admin123",
-  database: "mydb",
+// Configure the MySQL database connection
+const connection = mysql.createConnection({
+    host: "a3-database-instance-1.couyu5nfg2be.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "admin123",
+    database: "mydb",
+  });
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ', err);
+    return;
+  }
+  console.log('Connected to the database');
+
+  // Create the products table if it does not exist
+  const createTableSQL = `CREATE TABLE IF NOT EXISTS products (
+    name varchar(100),
+    price varchar(100),
+    availability boolean
+  )`;
+
+  connection.query(createTableSQL, (err) => {
+    if (err) {
+      console.error('Error creating the products table: ', err);
+      return;
+    }
+    console.log('Table "products" created or already exists');
+  });
 });
 
+// Parse JSON bodies
+app.use(bodyParser.json());
 
-mysql.createConnection(connectionConfig)
-    .then(async (connection) => {
-        console.log('Connected to MySQL database!');
-        await createProductsTable(connection);
-    })
-    .catch((err) => {
-        console.error('Error connecting to MySQL database:', err);
+// API endpoint: /store-products
+app.post('/store-products', (req, res) => {
+  // Extract the products array from the JSON body
+  const products = req.body.products;
+
+  // Check if products array exists and is not empty
+  if (!products || products.length === 0) {
+    return res.status(400).send('Invalid input');
+  }
+
+  // Prepare the SQL statement to insert records into the products table
+  const sql = 'INSERT INTO products (name, price, availability) VALUES ?';
+
+  // Prepare the data to be inserted
+  const values = products.map((product) => [
+    product.name,
+    product.price,
+    product.availability ? 1 : 0 // Convert boolean value to 0 or 1
+  ]);
+  // Execute the SQL statement
+  connection.query(sql, [values], (err, result) => {
+    if (err) {
+      console.error('Error inserting records: ', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    console.log(`Inserted ${result.affectedRows} records into the products table`);
+    return res.status(200).json({ message: 'Success.' });
+  });
+});
+
+// API endpoint: /list-products
+app.get('/list-products', (req, res) => {
+    // Prepare the SQL statement to select all records from the products table
+    const sql = 'SELECT * FROM products';
+  
+    // Execute the SQL statement
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.error('Error querying the products table: ', err);
+        return res.status(500).send('Internal Server Error');
+      }
+  
+      console.log(`Retrieved ${result.length} products from the products table`);
+      const products = result.map((row) => ({
+        name: row.name,
+        price: row.price,
+        availability: Boolean(row.availability),
+      }));
+      return res.status(200).json({ products });
     });
+  });
+  
 
-// Function to create "products" table if it doesn't exist
-async function createProductsTable(connection) {
-    try {
-        await connection.query(`
-            CREATE TABLE IF NOT EXISTS products (
-                name VARCHAR(100),
-                price DECIMAL(10, 2),
-                availability BOOLEAN
-            )
-        `);
-        console.log('Products table created or already exists.');
-    } catch (error) {
-        console.error('Error creating products table:', error);
-    }
-}
-
-app.use(express.json());
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-router.post('/store-products', async (req, res) => {
-    const { products } = req.body;
-
-    // Validate the products array
-    if (!Array.isArray(products)) {
-        return res.status(400).json({ error: 'Invalid products array' });
-    }
-
-    const connection = await mysql.createConnection(connectionConfig);
-
-    try {
-        const insertQuery = 'INSERT INTO products (name, price, availability) VALUES (?, ?, ?)';
-
-        for (const product of products) {
-            await connection.execute(insertQuery, [product.name, product.price, product.availability]);
-        }
-
-        console.log(`Inserted ${products.length} records into products table.`);
-        res.status(200).json({ message: 'Success' });
-    } catch (error) {
-        console.error('Error inserting records into products table:', error);
-        res.status(500).json({ error: 'Error inserting records into products table' });
-    } finally {
-        connection.end();
-    }
-});
-
-router.get('/list-products', async (req, res) => {
-    const connection = await mysql.createConnection(connectionConfig);
-
-    try {
-        const selectQuery = 'SELECT name, price, availability FROM products';
-        const [rows] = await connection.execute(selectQuery);
-
-        const products = rows.map((row) => ({
-            name: row.name,
-            price: row.price,
-            availability: row.availability === 1 ? true : false
-        }));
-
-        res.status(200).json({ products });
-    } catch (error) {
-        console.error('Error retrieving products:', error);
-        res.status(500).json({ error: 'Error retrieving products' });
-    } finally {
-        connection.end();
-    }
-});
-
-
-app.use(router);
-
-app.listen(80, function () {
-    console.log('Example app listening on port 3000!');
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
